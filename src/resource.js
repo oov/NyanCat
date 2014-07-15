61571d7dd6627c3233ee0ee49cd35d5067c8b60d
oov <ilovevipper@gmail.com>
2014-07-16 08:21:49 +0900

    nc.png を追加

--- src/resource.js
+++ src/resource.js
@@ -1,14 +1,16 @@
 var res = {
     HelloWorld_png : "res/HelloWorld.png",
     CloseNormal_png : "res/CloseNormal.png",
-    CloseSelected_png : "res/CloseSelected.png"
+    CloseSelected_png : "res/CloseSelected.png",
+    nc_png : "res/nc.png"
 };
 
 var g_resources = [
     //image
     res.HelloWorld_png,
     res.CloseNormal_png,
-    res.CloseSelected_png
+    res.CloseSelected_png,
+    res.nc_png
 
     //plist
 
